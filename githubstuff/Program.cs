eba872e3fdec7ea47898f5663816362f5746d1ee
Craig Graham <40495350@live.napier.ac.uk>
2020-10-13 11:44:32 +0100

    changed

---
+++ githubstuff/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace githubstuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
